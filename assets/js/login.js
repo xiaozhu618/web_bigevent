bf3e9a55765c32e0d40411e53df07b2b5def56c9
zhu_ming <1328449447@qq.com>
2020-09-13 18:32:13 +0800

    完成登录注册模块

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,4 +20,33 @@ $(function() {
               }
         }
     })
+    var layer = layui.layer;
+    $('#form_reg').on('submit',function(e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        console.log(data);
+        $.post('/api/reguser',data,function(res) {
+            if(res.status!= 0) {
+                return layer.msg(res.message);
+            }
+            console.log(res);
+            layer.msg('注册成功，请登录！');
+            $('#link_login').click();
+        })
+    })
+    $('#form_login').on('submit',function(e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.post('/api/login',data,function(res) {
+            if(res.status!=0){
+                return layer.msg('登录失败');
+            }
+            console.log(res);
+            layer.msg('登录成功！');
+            // 将登录成功得到的 token 字符串，保存到 localStorage 中
+            localStorage.setItem('token', res.token);
+            // 跳转到后台主页
+            location.href = '/index.html';                                                           
+        })
+    })
 })
